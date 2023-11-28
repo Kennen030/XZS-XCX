b5eb03c2e52886f178c6f15bb82065ced407eec3
Kennen zhou <2232404522@qq.com>
2023-11-28 13:22:32 +0800

    大文件

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-App({
-
-  /**
-   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
-   */
-  onLaunch: function () {
-    
-  },
-
-  /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
-   */
-  onShow: function (options) {
-    
-  },
-
-  /**
-   * 当小程序从前台进入后台，会触发 onHide
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
-   */
-  onError: function (msg) {
-    
-  }
-})
+version https://git-lfs.github.com/spec/v1
+oid sha256:94e57afab48a0f9145107a5bd8bbc0c78b1cd0fdb0444ffe5917b8398aca9616
+size 564
